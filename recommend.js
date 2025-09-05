e1a8f81c221dfa051efb1758a14dd33730f2c175
gooday9527 <gooday9527@gmail.com>
2025-09-05 14:57:16 +0800

    Update recommend.js

--- recommend.js
+++ recommend.js
@@ -2,8 +2,7 @@
 //                 recommend.js (最終動態版)
 // =================================================================
 
-const STATIC_JSON_URL = "https://cdn.jsdelivr.net/gh/gooday9527/member@main/data/recommendations_latest.json";
-https://member.gooday9527.com/data/recommendations_latest.json
+const STATIC_JSON_URL = "https://github.com/gooday9527/data/recommendations_latest.json";
 
 let isRecommendInitialized = false;
 let recommendDataCache = {}; // 格式：{ 分類名稱: [...] }
